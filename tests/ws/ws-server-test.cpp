472b333d060cfa7f78b33debb781b1f760686c7b
Sylvain Garcia <sylvain.garcia@non.se.com>
2022-08-01 16:47:42 +0200

    [ws] remove dead code

--- tests/ws/ws-server-test.cpp
+++ tests/ws/ws-server-test.cpp
@@ -12,57 +12,6 @@
 
 using namespace g6;
 
-// TEST_CASE("ws simple server", "[g6::web::ws]") {
-//     spdlog::set_level(spdlog::level::debug);
-
-//     web::context ctx{};
-//     std::stop_source stop_source{};
-
-//     auto server = web::make_server(ctx, web::proto::ws, *from_string<net::ip_endpoint>("127.0.0.1:0"));
-//     auto server_endpoint = *server.socket.local_endpoint();
-
-//     spdlog::info("server listening at: {}", server_endpoint);
-
-//     sync_wait(
-//         [&]() -> task<void> {
-//             co_await web::async_serve(server, stop_source.get_token(), [&] {
-//                 return [&stop_source]<typename Session>(Session session, std::stop_token stop_token) -> task<void> {
-//                     scope_guard _ = [&]() noexcept {//
-//                         stop_source.request_stop();
-//                     };
-//                     auto message = co_await net::async_recv(session);
-//                     while (net::has_pending_data(message)) {
-//                         std::array<std::byte, 256> data;
-//                         auto sz = co_await net::async_recv(message, data);
-//                         if (sz) {
-//                             auto sv_body = std::string_view{reinterpret_cast<const char *>(data.data()), *sz};
-//                             spdlog::info("body: {}", sv_body);
-//                             REQUIRE(sv_body == "Hello !");
-//                         } else {
-//                             spdlog::info("closed: {}", to_string(sz.error()));
-//                         }
-//                     }
-//                     co_await net::async_send(session, as_bytes(std::span{"OK !", 4}));
-//                 };
-//             });
-//         }(),
-//         [&]() -> task<void> {
-//             auto session = co_await net::async_connect(ctx, web::proto::ws, server_endpoint);
-//             co_await net::async_send(session, as_bytes(std::span{"Hello !", 7}));
-//             auto message = co_await net::async_recv(session);
-//             std::string body_str;
-//             while (net::has_pending_data(message)) {
-//                 std::array<std::byte, 256> data;
-//                 auto sz = co_await net::async_recv(message, data);
-//                 auto body_sv = std::string_view{reinterpret_cast<const char *>(data.data()), *sz};
-//                 body_str += body_sv;
-//             }
-//             spdlog::info("body: {}", body_str);
-//             REQUIRE(body_str == "OK !");
-//         }(),
-//         async_exec(ctx, stop_source.get_token()));
-// }
-
 #include <random>
 
 std::string make_random_string(size_t size) noexcept {
@@ -111,7 +60,7 @@ TEST_CASE("ws simple server: segmented", "[g6::web::ws]") {
         }() | async_with(stop_source.get_token()),
         [&]() -> task<void> {
             auto session = co_await net::async_connect(ctx, web::proto::ws, server_endpoint);
-            const std::string tx_body = make_random_string(1021);            
+            const std::string tx_body = make_random_string(1021);
             co_await net::async_send(session, tx_body);
             auto response = co_await net::async_recv(session);
             std::string rx_body;
